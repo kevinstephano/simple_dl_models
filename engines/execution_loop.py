3bc51302b13cb6f99dcfa8648e6ad4bc5e2b0d3f
Kevin Stephano <kevin.stephano@gmail.com>
2022-06-03 22:07:23 +0000

    Restructuring executor usage.  Step 1: make a new training loop.

---
+++ engines/execution_loop.py
@@ -0,0 +1,86 @@
+import torch
+import importlib
+import pip
+import subprocess
+import sys
+
+def pip_install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+class NullContext:
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+def execute(args, exec_name, model, optim_func, input_func, compare_record=None, grad_func=None) :
+    optimize_ctx = NullContext
+    if args.torchdynamo or args.aot_autograd:
+        try:
+            importlib.import_module('functorch')
+            print('Successfully imported functorch!')
+        except ModuleNotFoundError:
+            print("Installing functorch...")
+            pip_install('git+https://github.com/pytorch/functorch.git#egg=functorch[aot]')
+        finally:
+            if args.aot_autograd:
+                from functorch.compile import memory_efficient_fusion
+    if args.torchdynamo:
+        try:
+            importlib.import_module('torchdynamo')
+            print('Successfully imported torchdynamo!')
+        except ModuleNotFoundError:
+            print("Installing torchdynamo...")
+            pip_install('git+https://github.com/pytorch/torchdynamo.git#egg=torchdynamo')
+        finally:
+            import torchdynamo
+            from torchdynamo.optimizations.training import aot_autograd_speedup_strategy
+            optimize_ctx = torchdynamo.optimize(aot_autograd_speedup_strategy)
+
+    model.to(device=args.device)
+    model.to(dtype=args.model_dtype)
+
+    optimizer = optim_func(model.parameters())
+    scaler = torch.cuda.amp.GradScaler(enabled=(args.grad_scaler and not hasattr(optimizer, '_dummy')))
+    
+    if args.aot_aotugrad:
+        model = memory_efficient_fusion(model)
+    elif args.jit_script:
+        model = torch.jit.script(model)
+
+    batches = input_func(args.warmup_steps+args.steps, args.input_dtype, args.device)
+    grads = None
+    if grad_func :
+        grads = grad_func(args.warmup_steps+args.steps, args.input_dtype, args.device)
+
+    start_evt = torch.cuda.Event(enable_timing=True)
+    stop_evt = torch.cuda.Event(enable_timing=True)
+
+    with torch.autograd.profiler.emit_nvtx(enabled=args.profile_with_nvtx):
+        with torch.jit.fuser('fuser2'), optimize_ctx:
+            for step,batch in enumerate(batches) :
+                if step == args.warmup_steps :
+                    torch.cuda.profiler.start()
+                    start_evt.record()
+        
+                if not args.inference :
+                    with torch.cuda.amp.autocast(enabled=args.amp):
+                        loss = model(*batch)
+                    if grads :
+                        scaler.scale(loss).backward(grads[step])
+                    else :
+                        scaler.scale(loss).backward()
+                
+                    if step % args.grad_accum_steps == 0 :
+                        scaler.step(optimizer)
+                        scaler.update()
+                        optimizer.zero_grad(set_to_none=True)
+                else :
+                    with torch.inference_mode() :
+                        with torch.cuda.amp.autocast(enabled=args.amp):
+                            loss = model(*batch)
+   
+    stop_evt.record()
+    stop_evt.synchronize()
+    return start_evt.elapsed_time(stop_evt)
