8fead2c48ab7c0f64bc56311f0ff11381e0815bf
riship <riship@nvidia.com>
2022-07-06 15:02:42 -0700

    working on it

--- execution/execution_loop.py
+++ execution/execution_loop.py
@@ -30,10 +30,8 @@ def result_record(args, exec_name, model_name, exec_time, gpu_memory, compare_re
     }
 
 def forward_pass(model, batch):
-    if isinstance(batch, torch_geometric.data.Data):
-        return model(batch.x, batch.edge_index)
-    elif isinstance(batch, torch_geometric.data.HeteroData):
-        ret
+    if isinstance(batch, torch_geometric.data.Data) or isinstance(batch, torch_geometric.data.HeteroData):
+        return model(batch)
     else:
         return model(*batch)
 
